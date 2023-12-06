93aedc185b5f3409ab73f7f4e9047d690410c97a
t.nugent <t.nugent@hotmail.co.uk>
2023-12-06 10:45:17 +0000

    Models in place

--- LibraryWebProject/Books/models.py
+++ LibraryWebProject/Books/models.py
@@ -1,25 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    firstNames = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    def __str__(self):
+        return f"{self.lastName}, {self.firstNames}"
+
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return f"{self.name}"
+
 class Book(models.Model):
-    GENRES = [
-        ("ACTI", "Action"),
-        ("SCIF", "Science Fiction"),
-        ("FANT", "Fantasy"),
-        ("NONF", "Non Fiction"),
-        ("MYST", "Mystery"),
-    ]
     isbn = models.CharField(max_length=13, primary_key=True,)
     name = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200, choices=GENRES)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="authors")
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name="genres")
     def __str__(self):
-        return f"{self.isbn}: \"{self.name}\" by {self.author} ({self.get_GENRES_display})"
+        return f"{self.name} ({self.author}) \t {self.genre} \t ({self.isbn})"
     
 class BookCopy(models.Model):
+    id = models.AutoField(primary_key=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="copies")
-    copyId = models.IntegerField(primary_key=True,)
+    copyNumber = models.IntegerField()
     onLoan = models.BooleanField(default = False)
 
+    class Meta:
+        unique_together = ['book', 'copyNumber']
+
     def __str__(self):
-        return f"{self.book.name} (self.copyId): On Loan ({self.onLoan})"
+        return f"{self.book.name} ({self.copyNumber}): On Loan ({self.onLoan})"
+
+
+class Liar(models.Model):
+    id = models.AutoField(primary_key=True)
+    firstNames = models.CharField(max_length=200)
+
