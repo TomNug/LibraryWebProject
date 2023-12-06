8c2940ddb271cc36556427513691b4ba9f2876d3
t.nugent <t.nugent@hotmail.co.uk>
2023-12-06 10:45:24 +0000

    Models in place

--- LibraryWebProject/Books/urls.py
+++ LibraryWebProject/Books/urls.py
@@ -1,8 +1,12 @@
 # books / urls
 from django.urls import path
-
+from . import views
 # from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("book/<int:book_id>", views.book, name = "book")
+
     ]
 
+#     path("<int:book_id>/copies", views.book, name = "book")
